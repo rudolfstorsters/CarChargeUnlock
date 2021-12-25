94fbf1be018a92081fee58e5e4680aa58743cc89
rudolfstorsters <rudolfs.torsters@gmail.com>
2021-12-25 22:04:09 +0200

    added scan page

---
+++ client/components/pages/ScanPage/ScanPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ScanPage extends React.Component {
+    render() {
+        return (
+            <div>
+                Scan me
+            </div>
+        )
+    }
+}
