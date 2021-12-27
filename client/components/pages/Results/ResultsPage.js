50f5d55018337481682c0c2b862eaf2592764dfe
rudolfstorsters <rudolfs.torsters@gmail.com>
2021-12-27 19:55:16 +0200

    minor changes

--- client/components/pages/Results/ResultsPage.js
+++ client/components/pages/Results/ResultsPage.js
@@ -5,6 +5,13 @@ class ResultsPage extends Component {
     render() {
         return (
             <div className="resultsPage">
+                <div className="succesMark">
+                    <div className="ellipse">
+                        <div className="checkMark">
+                        </div>
+                    </div>
+                    <p>Success</p>
+                </div>
 
                 <div className="backgroundImage"></div>
             </div>
